36225e7dd908db83c462994cd87a4c21d42aa2a1
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-01-07 12:07:10 +0300

    new

---
+++ src/Task1/Num.java
@@ -0,0 +1,9 @@
+package Task1;
+
+public class Num {
+    public static void main(String[] args) {
+        int i=1;
+        int x= 10; // значение которое вводит пользователь
+        for (i<x ; i++);
+    }
+}
