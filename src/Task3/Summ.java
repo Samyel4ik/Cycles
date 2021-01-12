9a0b53bd28d8835f2f6e22db1c666638fbad90d7
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-01-12 14:11:38 +0300

    end

--- src/Task3/Summ.java
+++ src/Task3/Summ.java
@@ -3,10 +3,10 @@ package Task3;
 public class Summ {
     public static void main(String[] args) {
         double i = 1;
-        double sum = 0;
+        double sum = 1;
         int a = 2; // степень
-        for (i = 1; i <= 100; i++) {
-            sum = sum + Math.pow(i, a);
+        for (i = 1; i <= 200; i++) {
+            sum = sum * Math.pow(i, a);
         }
         System.out.println(sum);
     }
--- src/Task5/Mtmory.java
+++ src/Task5/Mtmory.java
@@ -3,8 +3,9 @@ package Task5;
 public class Mtmory {
     public static void main(String[] args) {
         //Вывести на экран соответствующий между символами и их численными обозначениями в памяти компьютера.
-        int c = 555;
-        char b = (char) c;
+
+        char b = 'a';
+        int c = b ;
 
 
         if (c > 0 || c <= 65535) {
